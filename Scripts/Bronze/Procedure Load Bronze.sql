dac596652871cb351653701437b29bd7347f7826
Naga Mahendra Polavarapu <mahendrachowdary36@gmail.com>
2026-02-26 06:49:18 +0530

    Create Procedure Load Bronze.sql

---
+++ Scripts/Bronze/Procedure Load Bronze.sql
@@ -0,0 +1,63 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+===============================================================================
+*/
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\mahen\Downloads\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\mahen\Downloads\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\mahen\Downloads\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Users\mahen\Downloads\datasets\source_erp\loc_a101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\mahen\Downloads\datasets\source_erp\cust_az12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\mahen\Downloads\datasets\source_erp\px_cat_g1v2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
